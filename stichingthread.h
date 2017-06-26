b8d467394911940045548ba1bc544aaf3fb52b9e
MaximilianBlack <maximilian.black.07@gmail.com>
2017-06-26 18:41:56 +0300

    Update stichingthread.h
    asd

--- stichingthread.h
+++ stichingthread.h
@@ -23,11 +23,8 @@ class StichingThread : public QObject
     Mat* bufferImg1 = new Mat[20];
     Mat* bufferImg2 = new Mat[20];
 
-//    Mat bufferImg1[];
-//    Mat bufferImg2[];
     int countBufferImage;
     int countDotArray[][3];
-    //[][0-кол-во векторов;1-id img1;2-id img2]
 
 public:
     StichingThread();
@@ -60,19 +57,7 @@ public slots:
     void createCompairArray(Mat bufferImg1[],Mat bufferImg2[]);
     void compairCountControlPoints(int array[][3]);
     int findDescriptors(Mat left_image, Mat right_image);
-    void showPoint2fVectors(vector<Point2f> v, string name);
-    void showPoint2IntVectors(vector<Point2i> v, string name);
-    void showKeyPointVectors(vector<KeyPoint> vLeft, vector<KeyPoint> vRight);
-    //vector<Point2i> changeBasicZero (vector<Point2f> v, int size);
-    vector<Point2f> changeBasicZero (vector<Point2f> v, int size);
-    vector<Point2f> changeVectorFloatToBinary(vector<Point2f> v);
-    vector<Point2i> changeVectorIntToBinary(vector<Point2i> v);
-    float changeFloatToBinary(float f);
-    int changeIntToBinary(int intCount);
     void setRunning(bool running);
-    void setImage(Mat image);
-    void setImage_2(Mat image2);
-    void useMatHomogeneus(vector<KeyPoint> keypoints_object,vector<KeyPoint> keypoints_scene,vector< DMatch > good_matches,Mat img_matches,Mat img_object);
 };
 
 #endif // STICHINGTHREAD_H
